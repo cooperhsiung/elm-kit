f497f0363f56bda336d8a2be6f810468c0d8025d
Cooper Hsiung <cooperhsiung@gmail.com>
2018-09-12 00:30:39 +0800

    imp

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,14 @@
+module Main exposing (..)
+
 import Browser
+
+import Browser.Dom as Dom
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 
 main =
-  Browser.sandbox { init = 0, update = update, view = view }
+  -- Browser.sandbox { init = 0, update = update, view = view }
+  text "Hello world!"
 
 type Msg = Increment | Decrement
 
